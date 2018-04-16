9def832f71ad3e37b2723cb7e73228555ce255aa
Matt Law <mattlawiscool@gmail.com>
2018-04-16 16:33:36 -0700

    finished with event calling and mobile formatting

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ var url = "https://randomuser.me/api/";
 var fullName = document.querySelector("#fullname");
 var userName = document.querySelector('username');
 var profilePic = document.querySelector('img');
+var emailAddress = document.querySelector("#email")
+var city = document.querySelector("#city")
+
 
 
 btn.addEventListener('click', function(){
@@ -11,21 +14,25 @@ btn.addEventListener('click', function(){
         return response.json()
     })
     .then(function(data){
-        console.log(data.results[0]);
-        console.log(data.results[0].picture.large)
-        //Name Section
         var firstName = data.results[0].name.first
         var lastName = data.results[0].name.last
         var uFirstName = firstName.charAt(0).toUpperCase() + firstName.slice(1)
         var uLastName = lastName.charAt(0).toUpperCase() + lastName.slice(1)
+        
         //Username
         var userName = data.results[0].login.username
         //pic
         var img = data.results[0].picture.large
+        //Email and location
+        var email = data.results[0].email
+        var curCity = data.results[0].location.city
+        var locationFormat = curCity.charAt(0).toUpperCase() + curCity.slice(1) 
         //Modify Dom
         fullName.innerText = `${uFirstName} ${uLastName}`;
-        username.innerText = userName
-        profilePic.src = img
+        username.innerText = userName;
+        profilePic.src = img;
+        emailAddress.innerText = email;
+        city.innerText = locationFormat;
         
     })
 })
